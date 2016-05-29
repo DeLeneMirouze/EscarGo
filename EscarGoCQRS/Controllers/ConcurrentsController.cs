836afe11b9339ec541425def887c19b43e3fe031
De Lène Mirouze <fmirouze@expertime.com>
2016-05-29 12:20:37 +0200

    Création de EscargoCQRS

---
+++ EscarGoCQRS/Controllers/ConcurrentsController.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Web.Mvc;
+using EscarGo.Models;
+using EscarGoLibrary.Models;
+
+namespace EscarGoCQRS.Controllers
+{
+    public class ConcurrentsController : CustomController
+    {
+        // GET: Concurrents
+        public ActionResult Index()
+        {
+            var concurrents = Builder.GetCompetitors();
+
+            return View(concurrents);
+        }
+
+        // GET: Concurrents/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null || id.Value == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DetailConcurrentViewModel vm = Builder.GetDetailConcurrentViewModel(id.Value);
+            if (vm.Concurrent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vm);
+        }
+
+        public ActionResult Bet(int idCourse, int idConcurrent)
+        {
+            if (idConcurrent == 0 || idConcurrent == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Builder.SetBet(idCourse, idConcurrent);
+            return Redirect("Details/" + idConcurrent.ToString());
+        }
+    }
+}
