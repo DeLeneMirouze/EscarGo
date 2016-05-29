43fe684b6ad1be36ed1f3271b0f64d013a2384aa
De Lène Mirouze <fmirouze@expertime.com>
2016-05-29 12:16:07 +0200

    Création du projet EscarGoAsync

---
+++ EscarGoAsync/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace EscarGoAsync
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ EscarGoAsync/Controllers/CoursesController.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Web.Mvc;
+using EscarGo.Models;
+using EscarGo.Repositories;
+
+namespace EscarGoAsync.Controllers
+{
+    public class CoursesController : CustomController
+    {
+        private EscarGoContext db = new EscarGoContext();
+
+        // GET: Courses
+        public ActionResult Index()
+        {
+            return View(CourseRepository.GetCourses());
+        }
+
+        // GET: Courses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null || id.Value == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var vm = Builder.GetDetailCourseViewModel(id.Value);
+            if (vm.Course == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vm);
+        }
+
+        public ActionResult Bet(int idCourse, int idConcurrent)
+        {
+            if (idConcurrent == 0 || idConcurrent == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Builder.SetBet(idCourse, idConcurrent);
+            return Redirect("Details/" + idCourse.ToString());
+        }
+    }
+}
