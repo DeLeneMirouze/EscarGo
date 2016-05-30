301d9344442095cbaab8ee1f5327769570dd3ffe
De Lène Mirouze <fmirouze@expertime.com>
2016-05-30 15:37:41 +0200

    web.config complété

---
+++ EscarGoLibrary/Repositories/AzureTableRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Threading.Tasks;
+
+namespace EscarGoLibrary.Repositories
+{
+    public sealed class AzureTableRepository: IAzureTableRepository
+    {
+        #region Constructeur
+        CloudTableClient _tableClient;
+
+        public AzureTableRepository()
+        {
+            CloudStorageAccount csa = CloudStorageAccount.Parse(_storageConnectionString);
+            _tableClient = csa.CreateCloudTableClient();
+        } 
+        #endregion
+
+        #region GetTable (private)
+        private async Task<CloudTable> GetTable(string tableName)
+        {
+            CloudTable table = _tableClient.GetTableReference(tableName);
+            await table.CreateIfNotExistsAsync();
+
+            return table;
+        }
+        #endregion
+
+
+        string _storageConnectionString = 
+            "DefaultEndpointsProtocol=https;AccountName=logisfrdev;AccountKey=3kyVPau3MShgsI35sIUPajjE9HtGmyQx3fIevTonN0xgEMyRIU4JUc2SOjrbfwfTNjzJmQpI1POWhCv33fcO1w==";
+    }
+}
