b1c13132cab1904313f145cd8f46abf866bf6e03
De Lène Mirouze <fmirouze@expertime.com>
2016-07-27 19:51:00 +0200

    CQRS competiteurs

---
+++ CQRSJobs/Functions.cs
@@ -0,0 +1,110 @@
+﻿#region using
+using EscarGoLibrary.Models;
+using EscarGoLibrary.Repositories;
+using EscarGoLibrary.Storage.Model;
+using EscarGoLibrary.Storage.Repository;
+using Microsoft.Azure.WebJobs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+#endregion
+
+namespace CQRSJobs
+{
+    public sealed class Functions
+    {
+        #region Constructeur
+        static EscarGoContext context;
+        static ICompetitorRepositoryAsync _competitorRepository;
+        static ITableStorageRepository _storage;
+        static IRaceRepositoryAsync _raceRepository;
+
+        static Functions()
+        {
+            context = new EscarGoContext();
+            _competitorRepository = new CompetitorRepository(context);
+            _storage = new TableStorageRepository();
+            _raceRepository = new RaceRepository(context);
+        } 
+        #endregion
+
+        #region ProcessCompetitors
+        [NoAutomaticTrigger]
+        public static async Task ProcessCompetitors(TextWriter log)
+        {
+            try
+            {
+                List<Concurrent> concurrents = await _competitorRepository.GetCompetitorsAsync();
+
+                List<CompetitorEntity> entities = new List<CompetitorEntity>();
+                foreach (Concurrent concurrent in concurrents)
+                {
+                    var hisRaces = await _competitorRepository.GetRacesByCompetitorAsync(concurrent.ConcurrentId);
+                    var paris = await _competitorRepository.GetBetsByCompetitorAsync(concurrent.ConcurrentId);
+
+                    foreach (Course race in hisRaces)
+                    {
+                        CompetitorEntity entity = concurrent.ToCompetitorEntity();
+                        entity.PartitionKey = concurrent.ConcurrentId.ToString();
+
+                        entity.Course = race.Label;
+                        entity.Date = race.Date;
+                        entity.RowKey = race.CourseId.ToString();
+                        entity.SC = paris.FirstOrDefault(p => p.CourseId == race.CourseId).SC;
+
+                        entities.Add(entity);
+                    }
+                }
+
+                _storage.SetCompetitors(entities);
+
+                await log.WriteLineAsync("Mise à jour du lot réussie");
+            }
+            catch (Exception ex)
+            {
+                await log.WriteLineAsync(ex.Message);
+            }
+        }
+        #endregion
+
+        #region ProcessRaces
+        [NoAutomaticTrigger]
+        public static async Task ProcessRaces(TextWriter log)
+        {
+            try
+            {
+                List<Course> courses = await _raceRepository.GetRacesAsync(0, 0);
+
+                List<RaceEntity> entities = new List<RaceEntity>();
+                foreach (Course course in courses)
+                {
+                    var hisCompetitors = await _raceRepository.GetConcurrentsByRaceAsync(course.CourseId);
+                    var paris = await _competitorRepository.GetBetsByRaceAsync(course.CourseId);
+
+                    foreach (Concurrent competitor in hisCompetitors)
+                    {
+                        RaceEntity entity = course.ToRaceEntity();
+                        entity.PartitionKey = course.CourseId.ToString();
+
+                        entity.Concurrent = competitor.Nom;
+                        entity.RowKey = competitor.ConcurrentId.ToString();
+                        entity.SC = paris.FirstOrDefault(p => p.ConcurrentId == competitor.ConcurrentId).SC;
+
+                        entities.Add(entity);
+                    }
+                }
+
+                _storage.SetRaces(entities);
+
+                await log.WriteLineAsync("Mise à jour du lot réussie");
+            }
+            catch (Exception ex)
+            {
+                await log.WriteLineAsync(ex.Message);
+            }
+        }
+        #endregion
+    }
+}
---
+++ CQRSJobs/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Azure.WebJobs;
+
+//http://blog.amitapple.com/post/2015/06/scheduling-azure-webjobs/#.V5TxvriLShc
+// http://stackoverflow.com/questions/36610952/azure-webjobs-vs-azure-functions-how-to-choose
+
+namespace CQRSJobs
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            var host = new JobHost();
+
+
+
+
+
+            //Task callTask = host.CallAsync(typeof(Functions).GetMethod("ProcessCompetitors"));
+            //Console.WriteLine("Waiting for ReturnBonusReminder async Task");
+
+            //callTask.Wait();
+            //Console.WriteLine("ReturnBonusReminder Task complete with status : {0}", callTask.Status);
+
+
+
+
+
+
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+        }
+    }
+}
+
+
+
